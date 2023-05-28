64a370ba151ffa363b05720c00a40ecbb54465f8
Chris Eakins <ceakins92@gmail.com>
2023-05-27 20:52:47 -0600

    Deleted bad file, corrected bugs, added comments

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
+//Import UUID============================================================
+
 import { v4 as uuidv4 } from "uuid"
+
+//Create User Type Including Cart========================================
 
 type User = {
     uid: string,
@@ -7,6 +11,8 @@ type User = {
     age: number,
     cart: Item[]
 }
+
+// Function to Create New User, with Empty Cart =========================
 
 function createUser(first_name:string,last_name:string,age:number):User{
     let userId = uuidv4()
@@ -19,12 +25,16 @@ function createUser(first_name:string,last_name:string,age:number):User{
     }
 }
 
+// Create Item Type ======================================================
+
 type Item = {
     iid: string,
     name: string,
     description: string,
     price: number
 }
+
+// Function to Create New Item ===========================================
 
 function createItem(name:string,description:string,price:number):Item{
     let itemId = uuidv4()
@@ -36,15 +46,22 @@ function createItem(name:string,description:string,price:number):Item{
     }
 }
 
+// Function to Add to Cart ===============================================
+
 function addToCart(user:User,item:Item):void{
     user.cart.push(item)
     console.log(`You added 1 ${item.name} to your cart.`)
 }
 
+// Function to Remove from Cart ===========================================
+
 function removeFromCart(user:User,item:Item):void {
     user.cart = user.cart.filter(function (cartItem) { return cartItem.iid !== item.iid; })
     console.log(`Removed all ${item.name}s from Cart.`)
 }
+
+// Fuction to Remove a Certain Quantity From Cart ========================
+
 function removeQuantityFromCart(user:User,item:Item, quantity:number):void {
     let remainingItems = [];
     let count = 0;
@@ -66,6 +83,8 @@ function removeQuantityFromCart(user:User,item:Item, quantity:number):void {
     console.log(`<- You removed ${quantity} ${item.name} from Cart.`)
 }
 
+// Function to View Cart Total ============================================
+
 function cartTotal(user: User): number {
     let totalCart: number = 0
     for (let item of user.cart) {
@@ -75,6 +94,8 @@ function cartTotal(user: User): number {
     return totalCart
 }
 
+// Function to Print Cart ================================================
+
 function printCart(user: User) {
     console.log(`${user.first_name}'s Cart:`);
     for (let _i = 0, _a = user.cart; _i < _a.length; _i++) {
@@ -82,6 +103,8 @@ function printCart(user: User) {
         console.log("-> ".concat(item.name, ": $ ").concat(`${item.price}`));
     }
 }
+
+// Driver Function with Simulated Input ==================================
 
 function driver(): void {
     const user1 = createUser('Chris','Eakins', 40)
@@ -111,7 +134,8 @@ function driver(): void {
 
 driver()
 
-/*
+
+/* Output from driver():  ==========================================
 
 You added 1 Beer to your cart.
 Chris's Cart:
